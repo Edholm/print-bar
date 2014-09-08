d7b1765ec81ffb1745d5cb457c468abf2dd5f8d0
Emil Edholm <emil@edholm.it>
2014-09-08 20:04:34 +0200

    Added command line arguments

--- print-bar.cpp
+++ print-bar.cpp
@@ -1,8 +1,11 @@
+/* Prints a simple bar to stdout, e.g.
+ * $ ./print-bar -w 10 -f ▣ -e ▢ 50
+ * Outputs: ▣▣▣▣▣▢▢▢▢▢
+ *
+ * Copyright 2014 Emil Edholm <emil@edholm.it>
+ */
 #include <iostream>
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <math.h>
+#include "boost/program_options.hpp"
 
 using namespace std;
 
@@ -26,26 +29,61 @@ void print_bar(int usage, int width, string tick_full, string tick_empty) {
     cout << bar;
 }
 
+bool flag_equal(char* flag, char* that) {
+    return strcmp(flag, that) == 0;
+}
+
 int main(int argc, char *argv[])
 {
-    if(argc != 2) {
-        fprintf(stderr, "Illegal number of arguments. Expected 1, got %i\n",
-                argc-1);
+    int width, usage;
+    string tick_full, tick_empty;
+
+    namespace po = boost::program_options;
+    po::options_description desc(" [options] <usage>");
+    desc.add_options()
+      ("help,h", "Print help message")
+      ("width,w", po::value<int>(&width)->default_value(BAR_WIDTH),
+                "The width of the bar, i.e. number of chars")
+      ("tick-full,f", po::value<string>(&tick_full)->default_value(DEFAULT_TICK_FULL),
+                    "The character that indicates usage")
+      ("tick-empty,e", po::value<string>(&tick_empty)->default_value(DEFAULT_TICK_EMPTY),
+                    "The character that indicates emptiness")
+      ("usage", po::value<int>(&usage)->required(),
+                "A usage integer between 0-100");
+
+    po::positional_options_description positionalOptions;
+    positionalOptions.add("usage", 1);
+
+    po::variables_map vm;
+
+    try {
+        po::store(po::command_line_parser(argc, argv).options(desc)
+                  .positional(positionalOptions).run(), vm);
+
+        if(vm.count("help")) {
+            cerr << argv[0] << desc << endl;
+            return 0;
+        }
+        po::notify(vm);
+    }
+    catch(boost::program_options::required_option& e)
+    {
+        std::cerr << "ERROR: " << e.what() << endl;
         return 1;
     }
-    if(!isdigit(argv[1][0])) {
-        fprintf(stderr, "You did not supply a correct usage integer. \n\
-                Expected value between 0-100. Got: %s\n", argv[1]);
+    catch(boost::program_options::error& e)
+    {
+        std::cerr << "ERROR: " << e.what() << endl;
         return 1;
     }
 
-    int usage = atoi(argv[1]);
-    if(usage < 0)
+
+    if(usage < 0) {
         usage = 0;
-    else if(usage > 100)
+    }
+    else if(usage > 100) {
         usage = 100;
-
-
-    print_bar(usage, BAR_WIDTH, DEFAULT_TICK_FULL, DEFAULT_TICK_EMPTY);
+    }
+    print_bar(usage, width, tick_full, tick_empty);
     return 0;
 }
